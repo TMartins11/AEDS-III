c7eb49751711224635bc3995891eb998e45642da
Thiago Martins <thiago.martins1112@gmail.com>
2025-04-08 21:34:05 -0300

    chore: inicia repositório com makefile, arquivos instância e descrição do projeto

---
+++ tp2/source/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*TODO: Implementação dos 3 algoritmos + configuração da biblioteca de testes.*/
+
+/*
+****************************************************DESCRIÇÃO DO PROJETO************************************************************ 
+Neste trabalho, foram implementados três diferentes algoritmos para a resolução do problema do Caixeiro Viajante, cada um obedecendo 
+a um paradigma de projeto de algoritmos, sendo eles:
+ - Backtracking
+ - Algoritmo Guloso
+ - Programação Dinâmica
+
+ os algoritmos são avaliados por 20 diferentes instâncias da biblioteca TSPLIB. 
+
+Alunos: Thiago Martins | Pedro Augusto | Jeann Victor | Nicolas Rodrigues
+
+************************************************************************************************************************************
+*/
+
+int main(){
+
+    return 0;
+}
