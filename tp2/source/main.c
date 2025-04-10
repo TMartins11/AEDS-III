4759589df9cd734ddc0bc3019e4a02717bd0499b
Thiago Martins <thiago.martins1112@gmail.com>
2025-04-10 09:33:59 -0300

    feat: adiciona leitura correta das cidades e corrige bug de comparação léxica.

--- tp2/source/main.c
+++ tp2/source/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-/*TODO: Implementação dos 3 algoritmos + leitura dos arquivos instância.*/
+// Struct para armazenar cada cidade.
+typedef struct{
+    int id;
+    double x;
+    double y;
+}City;
+
+// Função para calcular a distância euclidiana entre dois pontos. √((xb - xa)² + (yb-ya)²)
+double calc_dist(City a, City b){
+    double dx = pow((b.x - a.x), 2);
+    double dy = pow((b.y - a.y), 2);
+
+    return sqrt((dx + dy));
+}
+
+/*TODO: Implementação dos 3 algoritmos + matriz de distâncias*/
 
 /************************************************************************************************************************************
 *                                                  DESCRIÇÃO DO PROJETO                                                             
@@ -26,30 +43,41 @@ int main(int argc, char** argv){
         printf("\nA Quantidade de argumentos passada é inválida!");
         return 1;
     }
-
-    char path[] = "./instancias/";
     
-    char full_path[50];
-    int i = 0, j = 0;
+    char full_path[100] = "./instancias/";
+    strcat(full_path, argv[1]);
 
-    while(path[i] != '/0'){
-        full_path[j++] = path[i++];
-    }
-
-    i = 0;
-    while(argv[1][i] != '/0'){
-        full_path[j++] = argv[1][i++];
-    }
-
-    full_path[j] = '/0';
-    
     FILE *arch = fopen(full_path, "r");
     if(!arch){
         printf("\nO arquivo instância não foi aberto corretamente!");
         return 1;
     }
 
+    int dimension = 0;
+    char line[128];
+
+    while( fgets(line, sizeof(line), arch)){
+        if(strncmp(line, "DIMENSION", 9) == 0){
+            sscanf(line, "DIMENSION: %d", &dimension);
+        }
+        if(strncmp(line, "NODE_COORD_SECTION", 18) == 0){
+            break;
+        }
+    }
+
+    if(dimension == 0){
+        printf("\nErro: Dimensão não encontrada!");
+        return 1;
+    }
+
+    City *cities = malloc(sizeof(City) * dimension);
+    for(int i = 0; i < dimension; i++){
+        fscanf(arch, "%d %lf %lf", &cities[i].id, &cities[i].x, &cities[i].y);
+    }
+
     fclose(arch);
+
+    free(cities);
 
     return 0;
 }
