af8549ea4b8c6245fdf9a624a0c22e93993f2b68
Thiago Martins <thiago.martins1112@gmail.com>
2025-04-13 23:31:00 -0300

    chore: adicionada pasta separada para acomodar parte dos gráficos.

--- tp2/graficos/grafico.py
+++ tp2/graficos/grafico.py
@@ -5,25 +5,22 @@ import seaborn as sns
 # Lê o CSV original
 df = pd.read_csv("resultados.csv", header=None, names=["instancia", "algoritmo", "tempo", "custo"])
 
-# Converte os valores para número (só por segurança)
 df["tempo"] = pd.to_numeric(df["tempo"], errors="coerce")
 df["custo"] = pd.to_numeric(df["custo"], errors="coerce")
 
-# Instâncias únicas
 instancias = df["instancia"].unique()
 
-# Cria uma lista com entradas do "forca_bruta"
+# Cria uma lista para o algoritmo força bruta, visto que ele não é capaz de calcular em tempo viável nenhuma instância apresentada.
 brute_force_rows = []
 
 for inst in instancias:
     brute_force_rows.append({
         "instancia": inst,
         "algoritmo": "forca_bruta",
-        "tempo": 999999.0,           # Simulando um tempo "infinito"
-        "custo": float("inf")        # Ou pode usar 999999 também
+        "tempo": 999999.0,          
+        "custo": float("inf")        
     })
 
-# Adiciona ao DataFrame
 df = pd.concat([df, pd.DataFrame(brute_force_rows)], ignore_index=True)
 
 # Gráfico: tempo de execução
@@ -32,7 +29,7 @@ sns.barplot(data=df, x="instancia", y="tempo", hue="algoritmo")
 plt.title("Tempo de Execução por Instância e Algoritmo (TSP)")
 plt.ylabel("Tempo (segundos)")
 plt.xticks(rotation=45)
-plt.yscale("log")  # Escala logarítmica pra acomodar o 'forca_bruta'
+plt.yscale("log")  
 plt.tight_layout()
 plt.show()
 
